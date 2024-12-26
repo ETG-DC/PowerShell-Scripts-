c18108aef8ae98a7d21f16decea1b35eacd259aa
Dorian S Crouch <95877269+ETG-DC@users.noreply.github.com>
2024-12-26 12:24:34 -0500

    Delete AVD - Server Side GPO for enabling RemoteFX USB Redirection.ps1

--- AVD - Server Side GPO for enabling RemoteFX USB Redirection.ps1
+++
@@ -1,7 +0,0 @@
-# Remote Desktop "Server"
-$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
-Set-ItemProperty -Path $RegistryPath -Name "fEnableVirtualizedGraphics" -Value 1 -Type DWord
-Set-ItemProperty -Path $RegistryPath -Name "fDisablePNPRedir" -Value 0 -Type DWord
-GPUpdate /force
-Start-Sleep -s 10
-Restart-Computer -Force
